0657ee1b5abdb1ee23a235487714d34d307866a5
souzafilhoisac <souzafilhoisac@gmail.com>
2022-05-24 09:01:39 -0300

    terceiro exercício realizado

--- CineHouse/cinema.js
+++ CineHouse/cinema.js
@@ -1,7 +1,7 @@
 let cinema = "CineHouse"
 
 //console.log(cinema)
-
+//Array com objetos
 let catalogo = [
     {
         codigo: 221,
@@ -21,11 +21,27 @@ let catalogo = [
     }
 ]
 
-//console.log(catalogo.codigo)
+//catalogo.push({'codigo':null, 'titulo':null, 'duracao':null, 'atores':[null], 'anoLancamento':null, 'emCartaz':null})
+//console.log(catalogo)
+//Função para adicionar novos filmes com duas validações, a primeira verificando se o código 
+//e o titulo já estão no array, a segunda verificando se todos os campos estão devidamente preenchidos
 
-/*function adicionarFilmes(codigo, filme, duracao, atores, anoLancamento, emCartaz){
-    catalogo.push()
-}*/
+function adicionarFilmes(codigo, titulo, duracao, atores = [], anoLancamento, emCartaz){
+    for(let i = 0; i < catalogo.length; i++){
+        if(codigo === catalogo[i].codigo || titulo === catalogo[i].titulo) {
+            return "Código ou filme já cadastrado"            
+        } else if(emCartaz === undefined || emCartaz === null){
+            return "Por favor, preencha todos os campos."
+        }
+    }
+    catalogo.push({codigo, titulo, duracao, atores, anoLancamento, emCartaz})
+    console.log("Filme cadastrado com sucesso!")
+    return catalogo
+}
+
+//console.log(adicionarFilmes(25, "Homem Aranha", 2, ["Tobey Maguire", "Kirsten Dunst", "Willem Dafoe"], 2002, false))
+
+//Função que busca o filme pelo código retornando o título do mesmo.
 
 function buscarFilme(cod){
     for(let i = 0; i < catalogo.length; i++){
@@ -37,6 +53,8 @@ function buscarFilme(cod){
 }
 
 //console.log(buscarFilme(42))
+
+//Função que altera o status (em cartaz) de acordo o código do filme buscado.
 
 function alterarStatusEmCartaz(cod){
     for(let i = 0; i < catalogo.length; i++){
