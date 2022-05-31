f0ca47554416ab0dad57cbcc8df03263253379e4
souzafilhoisac <souzafilhoisac@gmail.com>
2022-05-31 15:01:30 -0300

    Exercitando métodos em string

---
+++ Introdução-a-Node-e-JavaScript/metodosDeString/metodosStrings.js
@@ -0,0 +1,33 @@
+let frase = "  sou programador js ";
+
+//length
+//cumprimento da string
+
+console.log(frase.length)
+
+//indexof
+//pesquisa na string o que está nos parametros e retorna a posição
+
+console.log(frase.indexOf('js'))
+
+//slice
+//pega um pedaço da string de acordo a posição solicitada
+
+console.log(frase.slice(2,17))
+console.log(frase)
+
+//trim
+//retira os espaços no inicio e fim da string
+
+console.log(frase.trim())
+
+//split
+//transforma a string em array, dividindo de acordo com o parametro passado
+
+console.log(frase.split("o"))
+
+//replace
+//substitui caracteres de acordo os parametros passados
+
+let newText = frase.replace('js','html')
+console.log(newText)
