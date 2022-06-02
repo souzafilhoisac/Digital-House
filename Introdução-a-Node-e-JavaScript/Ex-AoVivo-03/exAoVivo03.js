6ab442a884105ea0263035f2e0fefd86f20cd113
souzafilhoisac <souzafilhoisac@gmail.com>
2022-06-02 16:44:51 -0300

    Atualizando...

--- Introdução-a-Node-e-JavaScript/Ex-AoVivo-03/exAoVivo03.js
+++ Introdução-a-Node-e-JavaScript/Ex-AoVivo-03/exAoVivo03.js
@@ -6,21 +6,21 @@ function apresentaTodosOsFilmesSeparadosPorPontoVirgula() {
     console.log(listaDeFilmes.join('; '))
 }
 
-apresentaTodosOsFilmesSeparadosPorPontoVirgula()
+//apresentaTodosOsFilmesSeparadosPorPontoVirgula()
 
 // 1.1. Apresentar quantidade de filmes da lista.
 function quantidadeTotalDeFilmes() {
     console.log(`A quantidade de filmes é ${listaDeFilmes.length}.`)
 }
 
-quantidadeTotalDeFilmes()
+//quantidadeTotalDeFilmes()
 
 // 1.3. Buscar o nome do filme de acordo com seu índice.
 function buscarNomeDoFilmePeloIndice(indiceDoFilme) {
     console.log(listaDeFilmes[indiceDoFilme])
 }
 
-buscarNomeDoFilmePeloIndice(3)
+//buscarNomeDoFilmePeloIndice(0)
 
 // 1.4. Adiciona um novo filme a lista pelo seu nome.
 function adicionaUmNovoFilmePeloSeuNome(nomeDoFilme) {
@@ -28,19 +28,26 @@ function adicionaUmNovoFilmePeloSeuNome(nomeDoFilme) {
     console.log(listaDeFilmes)
 }
 
-adicionaUmNovoFilmePeloSeuNome('Homem aranha')
+//adicionaUmNovoFilmePeloSeuNome('Homem aranha')
 
 // 1.5. Atualiza um filme a partir do seu índice na lista.
-function atualizarUmFilmePeloSeuIndice(indiceDoFilme) {
-    
+function atualizarUmFilmePeloSeuIndice(indiceDoFilme,novoFilme) {
+    listaDeFilmes[indiceDoFilme] = novoFilme;
+    console.log(listaDeFilmes);
 }
+
+//atualizarUmFilmePeloSeuIndice(2, 'Homem Aranha')
 
 // 1.6. Listar todos os filmes com seu índice/posição na lista.
-function listarTodosOsFilmesComSeuIndiceNaLista() {
-
-}
+listaDeFilmes.forEach(function(nomeDoFilme, indiceDoFilme){
+    console.log(`${indiceDoFilme} ${nomeDoFilme}`)
+})
 
 // 1.7. Listar filmes por quantidade.
 function listarFilmesPorQuantidade(quantidadeDeFilmesDesejada) {
-
+    for(let i = 0; i < quantidadeDeFilmesDesejada; i++){
+            console.log(listaDeFilmes[i]);
+    }    
 }
+
+listarFilmesPorQuantidade(3)
