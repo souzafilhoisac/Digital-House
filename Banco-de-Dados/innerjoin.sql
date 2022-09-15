6c0946c9ca5eee281f1f416201b55eabc3d7e251
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-09-15 12:27:54 -0300

    usando outros métodos

--- Banco-de-Dados/innerjoin.sql
+++ Banco-de-Dados/innerjoin.sql
@@ -29,3 +29,39 @@ LEFT JOIN genres
 ON genres.id = movies.genre_id
 ORDER BY movies.title;
 
+SELECT * FROM series;
+
+SELECT title, release_date, end_date,
+DATEDIFF (end_date, release_date) AS Duração
+FROM series;
+
+SELECT first_name, last_name
+FROM actors
+WHERE LENGTH(first_name) > 6
+ORDER BY first_name ASC;
+
+SELECT COUNT(*) FROM episodes;
+
+SELECT * FROM seasons;
+
+SELECT series.title, COUNT(seasons.number)
+FROM series
+JOIN seasons
+ON series.id = seasons.serie_id
+GROUP BY series.title;
+
+SELECT genres.name, COUNT(genres.id) AS TOTAL_MOVIES
+FROM genres
+LEFT JOIN movies
+ON genres.id = movies.genre_id
+WHERE TOTAL_MOVIES >= 3
+GROUP BY genres.name
+ORDER BY genres.name;
+
+SELECT genres.name, COUNT(genres.id) AS TOTAL_MOVIES
+FROM genres
+JOIN movies
+ON genres.id = movies.genre_id
+GROUP BY genres.name
+HAVING TOTAL_MOVIES >= 3;
+
